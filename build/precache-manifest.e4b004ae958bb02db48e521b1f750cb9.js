92ed072901a586a889cdf4bd718ccbe39a3a6aaa
Tuan Nguyen <titan.ngvtuan@gmail.com>
2021-06-23 11:43:33 +0700

    no message

---
+++ build/precache-manifest.e4b004ae958bb02db48e521b1f750cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0dcd41e1fb53d1e0ed2462ab8e22eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e351d22d47608ec170c3",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "2e4df1f8334c591ec05a",
+    "url": "/static/js/2.7dbfdefd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7dbfdefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e351d22d47608ec170c3",
+    "url": "/static/js/main.4412eed4.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
